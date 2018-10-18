aab77b6441d0ae35bc8e5053797d3fd3c7a7a55e
Cheyin L <cheyinl@gmail.com>
2018-10-19 00:18:10 +0800

    able to test multiple-producer one-consumer

--- test/eval_consumer.py
+++ test/eval_consumer.py
@@ -2,8 +2,6 @@
 """ Consumer side of test program """
 
 import sys
-import os
-import datetime
 import time
 from json import loads as json_loads
 import logging
@@ -20,8 +18,9 @@ def main():
 	_log.info("queue folder: [%s]", _QFOLDER)
 	pq = PersistentQueueViaTextFolder(_QFOLDER, unserializer_callable=json_loads)
 	miss_dequeue = 0
-	last_fetch = None
+	last_fetches = {}
 	display_t = time.time() * 10
+	display_p = "-"
 	while True:
 		t_dq_s = time.time()
 		v = pq.dequeue()
@@ -32,20 +31,24 @@ def main():
 				miss_dequeue = 0
 				time.sleep(3)
 			continue
+		p_id = v["producer_id"]
 		sn = v["value"]
+		last_fetch = last_fetches.get(p_id)
 		if last_fetch is not None:
 			if (last_fetch + 1) != sn:
-				sys.stdout.write("\033[2K\r[\033[93mERR\033[0m]: serial not continue: [%r - %r]\n" % (last_fetch, sn))
+				sys.stdout.write("\033[2K\r[\033[93mERR\033[0m]: serial not continue: [%r - %r](%r)\n" % (last_fetch, sn, p_id))
 		current_t = time.time() * 10
-		if current_t - display_t > 2:
+		t_dq = current_t - display_t
+		if (t_dq > 5) or ((t_dq > 2) and (display_p != p_id)):
 			sys.stdout.write("\033[2K\rdequeue: (%r) %r (took %f)." % (
-					v["producer_id"],
+					p_id,
 					sn,
 					(t_dq_e - t_dq_s),
 			))
 			sys.stdout.flush()
 			display_t = current_t
-		last_fetch = sn
+			display_p = p_id
+		last_fetches[p_id] = sn
 
 
 if __name__ == '__main__':
