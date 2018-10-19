9dd7752311e9a74fdcc3c6458fe496aea77fb921
Cheyin L <cheyinl@gmail.com>
2018-10-19 15:12:00 +0800

    able to shift range start

--- test/eval_producer.py
+++ test/eval_producer.py
@@ -11,24 +11,37 @@ import logging
 from commonutil_fileio_persistentqueue.textfolder import PersistentQueueViaTextFolder
 
 _QFOLDER = "/tmp/persistq-test-1"
+RANGE_END = 8589934592
 
 _log = logging.getLogger(__name__)
 
 
+def get_options():
+	rng_start = 0
+	producer_id = datetime.datetime.now().strftime(r"P%d%H%M%S")
+	for arg in sys.argv[1:]:
+		try:
+			aux = int(arg)
+			if aux < RANGE_END:
+				rng_start = aux
+				continue
+		except Exception:
+			pass
+		producer_id = arg
+	return (rng_start, producer_id)
+
+
 def main():
 	logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-	try:
-		producer_id = sys.argv[1]
-	except Exception:
-		producer_id = datetime.datetime.now().strftime(r"P%d%H%M%S")
+	rng_start, producer_id = get_options()
 	try:
 		os.makedirs(_QFOLDER)
 	except Exception as e:
 		_log.warning("caught exception on making queue folder: %r", e)
 	_log.info("queue folder: [%s]", _QFOLDER)
-	_log.info("producer_id: [%s]", producer_id)
+	_log.info("producer_id: [%s], range-start: %d.", producer_id, rng_start)
 	pq = PersistentQueueViaTextFolder(_QFOLDER, serializer_callable=json_dumps)
-	for idx in xrange(8589934592):
+	for idx in xrange(rng_start, RANGE_END):
 		aux = {
 				"producer_id": producer_id,
 				"value": idx,
